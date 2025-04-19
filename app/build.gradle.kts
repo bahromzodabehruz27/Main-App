3adaad808b2b9441523a81549cf0488f63cb81f9
Behruz Bahromzoda <149923211+bahromzodabehruz27@users.noreply.github.com>
2025-04-19 12:28:40 +0500

    removing unused file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "tj.behruz.mainapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = project.findProperty("VERSION_CODE")?.toString()?.toInt() ?: 1
-        versionName = "1.1"
+        versionCode = (project.findProperty("VERSION_CODE")?.toString()?.toInt() ?: 1)
+        versionName = (project.findProperty("VERSION_NAME")?.toString() ?: "1.0.0")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
