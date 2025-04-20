56213c52d6458393322ef2c16649676f1aafd2a6
Behruz Bahromzoda <149923211+bahromzodabehruz27@users.noreply.github.com>
2025-04-20 20:18:24 +0500

    sending message telegram 22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "tj.behruz.mainapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = (project.findProperty("versionCode") as String?)?.toInt() ?: 1
-        versionName = project.findProperty("versionName") as String? ?: "1.0"
+        versionCode =  1
+        versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
